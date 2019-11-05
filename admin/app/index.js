da1b01eb307c107d1ff25afe551d5b61b714d3b8
pardeep <pardeep.pktapps@gmail.com>
2019-11-05 17:51:21 +0530

    FIX port 8080

--- admin/app/index.js
+++ admin/app/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const  bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json());
+
 
 let os = require('os')
-console.log(os.userInfo());
-
-console.log(__dirname);
+const osinfo = os.userInfo();
+console.log("Username:================>",osinfo.username);
+console.log("PATH:======>>>>>>", __dirname);
 app.get('/', (req,res) => {
     res.send(" i am admin...");    
 })
